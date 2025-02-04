45387d0f781524e47e52dec8ca66e403dfa6f925
abhishekvijaykumar94 <36844551+abhishekvijaykumar94@users.noreply.github.com>
2025-02-04 23:15:45 +0800

    @LiveTrading @MarketData @Updating Live market data consumers for redis and influx

--- algoFarmLive/market_data/kafka/consumers/smart_api_influx_data_consumer.py
+++ algoFarmLive/market_data/kafka/consumers/smart_api_influx_data_consumer.py
@@ -1,24 +1,37 @@
 import asyncio
-from algoFarmAdapter.market_data.live.kafka_consumers import InfluxMarketDataAsyncConsumer
-from algoLibs.dao import InfluxDBClientManager
-from algoLibs.file_processors import TokenMappingProcessor
-from algoLibs.market_operations.equity_market_data_influxdb_operations import EquityMarketDataInfluxDbOperations
-from algoLibs.utils import PropertyManager, CommonUtils
+
+from algoFarmAdapter.market_data.live.kafka_consumers import LiveMarketDataConsumer
+from algoLibs import PropertyManager, TickerSubscriptionQuery, QueryExecutorData, TickerSubscription, DataRepository
+from algoLibs.converters.equity_market_data_converter import EquityMarketDataConverter
+from algoLibs.utils import CommonUtils
 
 if __name__ == '__main__':
     api_key = PropertyManager.getValue('apikey')
-    live_market_data_kafka_topic = PropertyManager.getValue('smartApi.liveMarketData')
-    boostrap_servers = PropertyManager.getValue('boostrap.servers')
-    pickle_output_path = CommonUtils.getFilePathOutputDirectory()
-    market_data_operations = EquityMarketDataInfluxDbOperations()
-    batch_size=PropertyManager.getValue('batch_size')
-    token_mapping_processor = TokenMappingProcessor('Token_mapping.csv')
-    token_mapping_df = token_mapping_processor.get_token_to_symbol_df()
-    influxDb_client_manager = InfluxDBClientManager()
-    smart_api_influx_market_data_consumer = InfluxMarketDataAsyncConsumer(live_market_data_kafka_topic, boostrap_servers,
-                                                                          "smartApiLiveMarketData",token_mapping_df,
-                                                                          market_data_operations,influxDb_client_manager,int(batch_size))
+    live_market_data_kafka_topic = PropertyManager.getValue('smartApi_liveMarketData')
+    bootstrap_servers = PropertyManager.getValue('boostrap_servers')
+    pickle_output_path = CommonUtils.get_file_path_output_directory()
+    equity_market_data_converter = EquityMarketDataConverter()
+    data_repository = DataRepository()
+    batch_size = PropertyManager.getValue('batch_size')
+    ticker_subscription_query = TickerSubscriptionQuery(max_timestamp=True)
+    query_executor_data = QueryExecutorData("-3hr")
+    ticker_subscriptions: list[TickerSubscription] = data_repository.query(ticker_subscription_query,
+                                                                           query_executor_data)
+    token_symbol_map = {}
+    for ticker_subscription in ticker_subscriptions:
+        token_symbol_map[ticker_subscription.token] = ticker_subscription.ticker_symbol
+
+    smart_api_influx_market_data_consumer = LiveMarketDataConsumer(kafka_topic=live_market_data_kafka_topic,
+                                                                       bootstrap_servers=bootstrap_servers,
+                                                                       kafka_group_id="smartApiLiveMarketData",
+                                                                       token_symbol_map=token_symbol_map,
+                                                                       retention_seconds=10800,
+                                                                       batch_size=int(batch_size),
+                                                                       persist_to_cache=False,
+                                                                       persist_to_influx=True
+                                                                       )
     print("Initiating Influx Market Data Consumer on Topic:", live_market_data_kafka_topic)
     asyncio.run(smart_api_influx_market_data_consumer.consume())
+
 
 
--- algoFarmLive/market_data/kafka/consumers/smart_api_redis_market_data_consumer.py
+++ algoFarmLive/market_data/kafka/consumers/smart_api_redis_market_data_consumer.py
@@ -1,8 +1,9 @@
 import asyncio
 
-from algoFarmAdapter.market_data.live.kafka_consumers import LiveMarketDataRedisConsumer
+from algoFarmAdapter.file_processors.token_mapping_processor import TokenMappingProcessor
+from algoFarmAdapter.market_data.live.kafka_consumers import LiveMarketDataConsumer
 from algoLibs.converters.equity_market_data_converter import EquityMarketDataConverter
-from algoLibs.file_processors import TokenMappingProcessor
+
 
 from algoLibs.utils import  CommonUtils
 from algoLibs.utils.property_manager import PropertyManager
@@ -17,7 +18,7 @@ if __name__ == '__main__':
     token_processor = TokenMappingProcessor('Token_mapping.csv',
                                             CommonUtils.get_file_path_from_data_directory('Token_mapping.csv'))
     token_symbol_map = token_processor.get_token_to_symbol_dict()
-    smart_api_redis_market_data_consumer = LiveMarketDataRedisConsumer(live_market_data_kafka_topic, bootstrap_servers,
+    smart_api_redis_market_data_consumer = LiveMarketDataConsumer(live_market_data_kafka_topic, bootstrap_servers,
                                                                       "smartApiLiveMarketData", equity_market_data_converter,
                                                                       token_symbol_map,
                                                                       retention_seconds=10800, batch_size=int(batch_size))
